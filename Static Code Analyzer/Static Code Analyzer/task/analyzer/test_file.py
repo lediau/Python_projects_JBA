1592486ea7192d4d4905af18fbbda51e768cf736
Ledio Deda <33701673+lediau@users.noreply.github.com>
2023-08-14 02:52:44 +0300

    Add files via upload

---
+++ Static Code Analyzer/Static Code Analyzer/task/analyzer/test_file.py
@@ -0,0 +1,12 @@
+def function_Name(argOne, arg_Two=[]):
+    varName = 1
+    var_another_name = varName + 1
+    varName = 2
+    print(var_another_name)
+
+
+def fun1(S=5, test=[]):  # default argument value is mutable
+    VARIABLE = 10
+    string = 'string'
+    varName = 2
+    print(VARIABLE)
